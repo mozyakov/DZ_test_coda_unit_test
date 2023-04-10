ea98a5eeb3dde8738776b9c5b380315236026058
mozyakov <s.mozyakov@gmail.com>
2023-04-10 22:58:04 +0700

    тест срабатывает не каждый раз

--- src/test/java/StatServiceTest.java
+++ src/test/java/StatServiceTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.security.Provider;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatServiceTest {
+    @Test                       //всегда начинать с аннотации
+    public void testAverage() {     //всегда такие модификаторы доступа ниже три этапа теста
+        StatService service = new StatService();      //1 - подготовка
+        int[] sales = {1, 2, 3, 4, 5, 6, 7, 8, 9};    //1 - подготовка
+
+        int actual = service.averageSale(sales);      //2 - шаги теста
+        int expected = (1+2+3+4+5+6+7+8+9) / sales.length;
+
+        Assertions.assertEquals(expected, actual);    //3 - сравнение ожидаемого с получившемся
+    }
 
 }
