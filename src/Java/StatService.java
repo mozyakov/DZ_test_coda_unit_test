ce9bbc31cd9e97ac608f45e1ac4bf17016a387ea
mozyakov <s.mozyakov@gmail.com>
2023-04-10 21:19:09 +0700

    1

--- src/Java/StatService.java
+++ src/Java/StatService.java
@@ -1,2 +1,9 @@
 public class StatService {
+    public int averageSale(int[] sales) {
+        int totalPrice = 0;
+        for (int sale : sales) {
+            totalPrice += sale;
+        }
+        return totalPrice / sales.length;
+    }
 }
